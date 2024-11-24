79bfab2d63aa88674e40e8ddaf8e554d99f09b36
RaushanKumar606 <roshankumarsingh964@gmail.com>
2024-11-24 20:15:07 +0530

    s

--- Store/index.js
+++ Store/index.js
@@ -16,7 +16,7 @@ const servicePost= require('./src/router/AllPost');
 
 
 const  corsOptions = {
-  origin: "http://localhost:5174",
+  origin: "http://localhost:5173",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
  
